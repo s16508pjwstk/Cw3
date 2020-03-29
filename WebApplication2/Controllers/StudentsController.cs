18a43992e7426ab28b56c6d8a3ba2b62a7d4f297
kbernatjanuszkiewicz <kbernatjanuszkiewicz@finevare.com>
2020-03-29 15:41:55 +0300

    initial commit

---
+++ WebApplication2/Controllers/StudentsController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.DAL;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{   
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        //[HttpGet]
+        //public string GetStudents(string orderBy)
+        //{
+        //    return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+        //}
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            } else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id)
+        {
+            return Ok("Aktualizacja dokonczona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("Usuwanie zakonczone");
+        }
+
+    }
+}
---
+++ WebApplication2/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Student
+    {
+        public int IdStudent
+        {
+            get; set;
+        }
+
+        public string FirstName
+        {
+            get; set;
+        }
+
+        public string LastName
+        {
+            get; set;
+        }
+
+        public string IndexNumber
+        {
+            get; set;
+        }
+    }
+}
