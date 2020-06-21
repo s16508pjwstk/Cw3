272113472ef1f083d0c9e29ea4a93fa7308dcfc1
Kacper Bernat-Januszkiewicz <kbernatjanuszkiewicz@finitec.pl>
2020-06-21 21:45:05 +0200

    done

--- WebApplication2/Service/SqlServerDbService.cs
+++ WebApplication2/Service/SqlServerDbService.cs
@@ -50,89 +50,11 @@ namespace WebApplication2.Service
             };
             db.Student.Add(student_enrollment);
 
-            //db.SaveChanges();
-
             return enrollment_tran;
-
-
-
-
-            //Enrollment enrollment = new Enrollment();
-            //using (SqlConnection connection = new SqlConnection(connectionString))
-            //{
-            //    connection.Open();
-            //    SqlCommand command = connection.CreateCommand();
-            //    SqlTransaction transaction;
-
-            //    transaction = connection.BeginTransaction("SampleTransaction");
-
-            //    command.Connection = connection;
-            //    command.Transaction = transaction;
-            //    try
-            //    {
-            //        command.CommandText = "select IdStudy from Studies where name = @name";
-            //        command.Parameters.AddWithValue("name", studentEnrollmentForm.Studies);
-
-            //        var dr = command.ExecuteReader();
-            //        if (!dr.Read())
-            //        {
-            //            dr.Close();
-            //            transaction.Rollback();
-            //            Console.WriteLine("Requested study doesn't exist!");
-            //            throw new NullReferenceException();
-            //        }
-            //        int idstudy = (int)dr["IdStudy"];
-            //        Console.WriteLine("idstudy: " + idstudy);
-            //        dr.Close();
-
-            //        command.CommandText = "select IdEnrollment from Enrollment where IdStudy = @id and Semester = 1";
-            //        command.Parameters.AddWithValue("id", idstudy);
-            //        var dr2 = command.ExecuteReader();
-            //        DateTime dateNow = DateTime.Now;
-            //        if (!dr2.Read())
-            //        {
-            //            command.CommandText = "insert into Enrollment (IdEnrollment, Semester, IdStudy, StartDate) values ((SELECT ISNULL(MAX(IdEnrollment) + 1, 1) FROM Enrollment), 1, @id, @date)";
-            //            command.Parameters.AddWithValue("id", idstudy);
-            //            command.Parameters.AddWithValue("date", dateNow);
-            //            command.ExecuteNonQuery();
-            //        }
-            //        int idEnrollment = (int)dr2["IdEnrollment"];
-            //        Console.WriteLine("IdEnrollment: " + idstudy);
-            //        dr2.Close();
-
-            //        command.CommandText = "insert into Student (IndexNumber, FirstName, LastName, BirthDate, IdEnrollment) values (@indexNumber, @firstName, @lastName, @birthDate, @idEnrollment)";
-            //        command.Parameters.AddWithValue("indexNumber", studentEnrollmentForm.IndexNumber);
-            //        command.Parameters.AddWithValue("firstName", studentEnrollmentForm.FirstName);
-            //        command.Parameters.AddWithValue("lastName", studentEnrollmentForm.LastName);
-            //        command.Parameters.AddWithValue("birthDate", Convert.ToDateTime(studentEnrollmentForm.BirthDate));
-            //        command.Parameters.AddWithValue("idEnrollment", idEnrollment);
-            //        command.ExecuteNonQuery();
-
-            //        enrollment.IdEnrollment = idEnrollment;
-            //        enrollment.StartDate = DateTime.Now.ToString();
-            //        enrollment.IdStudy = idstudy;
-            //        enrollment.Semester = 1;
-
-            //        transaction.Commit();
-            //        connection.Close();
-
-            //    }
-            //    catch (InvalidOperationException)
-            //    {
-            //        transaction.Rollback();
-            //    }
-            //    catch (SqlException e)
-            //    {
-            //        transaction.Rollback();
-            //        throw e;
-            //    }
-            //}
-            //return enrollment;
         }
 
         public Enrollment PromoteStudents(int semester, string studies)
         {
-            //Enrollment enrollment = new Enrollment();
             var db = new apbd1Context();
 
             var study = db.Studies.Where(e => e.Name.Equals(studies)).FirstOrDefault();
